2bbffae4da1b10518915b46b52745fe3fd06951e
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-08-28 21:29:22 -0400

    refactored how to determine which type of app we are dealing with from switch statement to look up in hashmap. Also added delete app functionality

--- frontend/src/components/AppTrackPage/AppTrackPage.tsx
+++ frontend/src/components/AppTrackPage/AppTrackPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header/Header";
 import TrackingContent from "./TrackingContent/TrackingContent";
+import Footer from "../Shared/Footer/Footer";
 
 const AppTrackPage: React.FC = () => {
 	return (
 		<>
 			<Header />
 			<TrackingContent />
+			<Footer />
 		</>
 	);
 };
--- frontend/src/components/AppTrackPage/TrackingContent/Application.tsx
+++ frontend/src/components/AppTrackPage/TrackingContent/Application.tsx
@@ -1,26 +1,43 @@
 import styles from "./Application.module.css";
 import React from "react";
+import LongMenu from "../../Shared/LongMenu";
 
 interface ApplicationProps {
 	company: string;
 	position: string;
 	date: string;
-	imageUrl: string;
+	status: string;
+	id: string;
+	deleteApp: Function;
 }
 
 const Application: React.FC<ApplicationProps> = (props) => {
 	return (
 		<div className={styles.application}>
-			<div className={styles.logoAndName}>
+			<div className={styles.container}>
 				<img
 					className={styles.companyLogo}
-					src={props.imageUrl}
+					src={`https://logo.clearbit.com/${props.company.replace(
+						/\s/g,
+						""
+					)}.com`}
 					alt="company photo"
 				/>
 				<h3 className={styles.companyName}>{props.company}</h3>
 			</div>
-			<p className={styles.positionName}>{props.position}</p>
-			<p className={styles.dateApplied}>{props.date}</p>
+			<div className={styles.container}>
+				<div>
+					<p className={styles.positionName}>{props.position}</p>
+					<p className={styles.dateApplied}>{props.date}</p>
+				</div>
+				<div className={styles.temp}>
+					<LongMenu
+						deleteApp={props.deleteApp}
+						status={props.status}
+						id={props.id}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
