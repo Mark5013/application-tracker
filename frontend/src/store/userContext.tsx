2652b90a78375983e1879598282de97067649c47
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2023-04-29 23:37:23 -0400

    created userContext file

---
+++ frontend/src/store/userContext.tsx
@@ -0,0 +1,57 @@
+import { useState, createContext } from "react";
+
+const UserContext = createContext({
+	user: {
+		id: -1,
+		email: "",
+		firstName: "",
+		lastName: "",
+		isLoggedIn: false,
+	},
+	login: (
+		id: number,
+		email: string,
+		firstName: string,
+		lastName: string
+	) => {},
+	logout: () => {},
+});
+
+export const UserContextProvider = (props: any) => {
+	const [user, setUser] = useState({
+		id: -1,
+		email: "",
+		firstName: "",
+		lastName: "",
+		isLoggedIn: false,
+	});
+
+	const login = (
+		id: number,
+		email: string,
+		firstName: string,
+		lastName: string
+	) => {
+		setUser({ id, email, firstName, lastName, isLoggedIn: true });
+	};
+
+	const logout = () => {
+		setUser({
+			id: -1,
+			email: "",
+			firstName: "",
+			lastName: "",
+			isLoggedIn: false,
+		});
+	};
+
+	const UserContextValue = { user, login, logout };
+
+	return (
+		<UserContext.Provider value={UserContextValue}>
+			{props.children}
+		</UserContext.Provider>
+	);
+};
+
+export default UserContext;
